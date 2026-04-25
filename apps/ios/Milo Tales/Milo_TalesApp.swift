69e8105bff2d08249822a81be0c3d56580727b12
vaisakh b <vaisakh@vaisakhs-MacBook-Pro.local>
2026-04-25 13:26:08 +0530

    init

---
+++ apps/ios/Milo Tales/Milo_TalesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Milo_TalesApp.swift
+//  Milo Tales
+//
+//  Created by vaisakh b on 25/04/26.
+//
+
+import SwiftUI
+
+@main
+struct Milo_TalesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
